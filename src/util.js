57d4c08b1d82d61fa8d63f75092b7c65343f0c58
bakapear <proxybomb@gmail.com>
2022-11-21 18:33:34 +0100

    fix formatTime

--- src/util.js
+++ src/util.js
@@ -8,19 +8,31 @@ module.exports = {
     }
   },
   formatTime (ms, decimals = 3) {
-    if (!ms) return null
+    if (!ms) return '0:00' + (decimals ? '.' + '0'.repeat(decimals) : '')
+
+    let invert = false
+
+    if (ms < 0) {
+      invert = true
+      ms = Math.abs(ms)
+    }
+
     ms = ms / 1000
     let s = Math.floor(ms % 60)
     let m = Math.floor(ms / 60 % 60)
     let h = Math.floor(ms / 60 / 60)
+
     if (!h) h = null
     else if (!m) m = '00'
     if (!s) s = '00'
-    let t = [h, m, s].filter(x => x !== null).map((x, i) => {
+
+    let t = [h, m, s].filter(x => x !== null).map((x, i, a) => {
       return (i !== 0 && x < 10 && x !== '00') ? '0' + x : x
     })
-    ms = ms.toString()
-    return t.join(':') + (decimals ? ms.substr(ms.indexOf('.'), decimals + 1) : '')
+
+    if (decimals) ms = (ms % 1).toString()
+
+    return (invert ? '-' : '') + t.join(':') + (decimals ? ms.substr(ms.indexOf('.'), decimals + 1) : '')
   },
   maxLen (str, len) {
     if (str.length > len) str = str.slice(0, len - 3).trim() + '...'
